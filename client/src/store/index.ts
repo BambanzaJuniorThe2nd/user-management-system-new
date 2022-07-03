170597ead21273a9ba359bb5b4e74dbe8f86e967
Jason Mahirwe <bjthe2nd@gmail.com>
2022-07-03 15:43:01 +0200

    Define store variables and functionalities for managing users, app messages

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,44 +1,83 @@
 import { ref, computed } from "vue";
+import { AppMessage, User } from "../core";
 
+/**
+ * Bundle and expose variables/functionalities 
+ * to do with the managing of other users in the system
+ */
 function initUsers() {
-    const projects = ref<User[]>([]);
-    const currentProjectId = ref("");
-    const message = ref<Message>({ type: "", message: "" });
+    const users = ref<User[]>([]);
+    const currentlySelectedUserId = ref("");
   
-    function setProjects(list: Project[]) {
-      projects.value = list;
+    function setUsers(list: User[]) {
+      users.value = list;
     }
   
-    function addProject(project: Project) {
-      projects.value.push(project);
+    function addUser(user: User) {
+        users.value.push(user);
     }
   
-    function setCurrentProject(id: string) {
-      currentProjectId.value = id;
+    function setCurrentlySelectedUser(id: string) {
+        currentlySelectedUserId.value = id;
     }
   
-    const currentProject = computed(() =>
-      projects.value.find((p) => p._id === currentProjectId.value)
+    const currentlySelectedUser = computed(() =>
+      users.value.find((p) => p._id === currentlySelectedUserId.value)
     );
   
-    function setMessage(newMessage: Message) {
-      message.value = newMessage;
+    return {
+        setUsers,
+        addUser,
+        setCurrentlySelectedUser,
+        currentlySelectedUser,
+        currentlySelectedUserId,
+        users,
+    };
+}
+
+/**
+ * Bundle and expose variables/functionalities 
+ * to do with app-wide message management
+ */
+function initAppMessage() {
+    const message = ref<AppMessage>({ type: "", message: "" });
+
+    function setMessage(newMessage: AppMessage) {
+        message.value = newMessage;
+    }
+
+    return {
+        message,
+        setMessage,
+    };
+}
+
+/**
+ * Bundle and expose variables/functionalities 
+ * to do with the managing of the currently logged-in admin
+ */
+function initAdmin() {
+    const admin = ref<User>();
+  
+    function setAdmin(user: User) {
+        admin.value = user;
+    }
+  
+    function unsetAdmin() {
+        admin.value = undefined;
     }
   
     return {
-      setProjects,
-      addProject,
-      setCurrentProject,
-      setMessage,
-      currentProject,
-      currentProjectId,
-      projects,
-      message,
+        setAdmin,
+        unsetAdmin,
+        admin,
     };
 }
 
 const store = {
-    ...initUsers()
+    ...initAdmin(),
+    ...initUsers(),
+    ...initAppMessage(),
 };
   
 export default store;
