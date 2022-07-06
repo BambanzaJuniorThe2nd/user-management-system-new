8ae6111aa9a571e6bfcc75744f2d3ec8e98b572f
Jason Mahirwe <bjthe2nd@gmail.com>
2022-07-06 14:27:44 +0200

    Use toast features to display app-wide messages on every client page

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,50 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { defineComponent, watch } from "vue";
+import store from "./store";
+import { Message } from "./core";
+import { toast } from "tailwind-toast";
 
+export default defineComponent({
+  setup() {
+    watch(store.message, (value) => {
+      if (value && value.type) {
+        displayToast(value);
+      }
+    });
+
+    const displayToast = (m: Message) => {
+      const defaultValues = {
+        shape: "square",
+        duration: 4000,
+        speed: 1000,
+        positionX: "center",
+        positionY: "top",
+        color: "white",
+      };
+
+      const { type, message } = m;
+
+      if (type === "error") {
+        toast()
+          .danger("", message)
+          .with({
+            ...defaultValues,
+            color: "bg-red-500",
+          })
+          .show();
+      } else {
+        toast()
+          .success("", message)
+          .with({
+            ...defaultValues,
+            color: "bg-green-500",
+          })
+          .show();
+      }
+    };
+  },
+});
 </script>
--- client/src/pages/auth/login.vue
+++ client/src/pages/auth/login.vue
@@ -1,3 +1,42 @@
 <template>
   <div>This is the login page</div>
 </template>
+<script lang="ts">
+import { defineComponent, reactive, onMounted } from "vue";
+import { surixClient } from "../../api";
+import { useRouter } from "vue-router";
+import { getAccessToken } from "../../core/auth";
+import store from "@/store";
+
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      if (getAccessToken()) {
+        router.push({ name: "dashboard" });
+      }
+    });
+
+    const loginCreds = reactive({ email: "", password: "" });
+
+    const login = async () => {
+      try {
+        await surixClient().login({
+          email: loginCreds.email,
+          password: loginCreds.password,
+        });
+        router.push({ name: "dashboard" });
+      } catch (e) {
+        store.setMessage({ type: "error", message: e.message });
+      }
+    };
+
+    return {
+      loginCreds,
+      login,
+    };
+  }
+
+});
+</script>
