f27a5308635156bd4f4a002bf38d0966c92ff41d
Jason Mahirwe <bjthe2nd@gmail.com>
2022-07-17 14:40:12 +0200

    Add routes for adding, editing, and deleting users

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -1,12 +1,36 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import MainPage from "../pages/main/main-page.vue";
+import AddPage from "../pages/add/add-page.vue";
+import EditPage from "../pages/add/edit-page.vue";
+import DeletePage from "../pages/add/delete-page.vue";
+import ChangePasswordPage from "../pages/add/change-password-page.vue";
 import Login from "../pages/auth/login.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
         name: "main",
-        component: MainPage,
+        component: MainPage
+    },
+    {
+        path: "/users/add",
+        name: "add",
+        component: AddPage,
+    },
+    {
+    path: "/users/edit/:id",
+    name: "edit",
+    component: EditPage,
+    },
+    {
+    path: "/users/delete/:id",
+    name: "delete",
+    component: DeletePage,
+    },
+    {
+    path: "/users/change/password",
+    name: "change",
+    component: ChangePasswordPage,
     },
     {
         path: "/auth/login",
